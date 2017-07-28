bc10e1c63bcce172d2d1aeda6fa8349d55866e31
Mattielew <matthew.lewis.t@gmail.com>
2017-07-28 16:06:47 -0700

    Ginsberg

--- python_fundamentals/HW/HW20.py
+++ python_fundamentals/HW/HW20.py
@@ -23,12 +23,14 @@
 from sys import argv
 
 script, input_file = argv
+#Ginsberg_I.txt
+
 
 def print_all(f):
     print(f.read())
 
 def rewind(f):
-    f.seek(0)
+    f.seek(210)
 
 def print_a_line(line_count, f):
     print(line_count, f.readline())
@@ -47,9 +49,13 @@ print("Let's print three lines:")
 
 current_line = 1
 print_a_line(current_line, current_file)
+#angelheaded hipsters burning for the ancient heavenly connection to the starry dynamo in the machinery of night,
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+#who poverty and tatters and hollow-eyed and high sat up smoking in the supernatural darkness of cold-water flats
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
+#floating across the tops of cities contemplating jazz,
+
