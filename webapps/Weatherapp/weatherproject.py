ce1aba9981c63caa14051b4e2e47b3ce5fcb3703
Mattielew <matthew.lewis.t@gmail.com>
2018-04-27 22:16:29 -0700

    initial push

---
+++ webapps/Weatherapp/weatherproject.py
@@ -0,0 +1,32 @@
+!/usr/bin/python
+#-*- coding: utf-8 -*-
+#===============================================================================
+#
+#         FILE: weatherapp.py
+#
+#        USAGE: ./ 
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Matthew Lewis 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: Fri Apr 27 22:04:51 PDT 2018
+#     REVISION: ---
+#===============================================================================
+
+#Process 
+#touch
+#vim
+#r!date 
+
+
+#import requests
+
+print("Content-Type: text/plain")
+print("")
+print("Congratulations, it's a web app!")
