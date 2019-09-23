68f50c16a48b71adf1572f5b8cfa09bf502c3b69
M L <matthew.lewis.t@gmail.com>
2019-09-23 14:32:46 -0700

    Update

--- python_fundamentals/dedup.py
+++ python_fundamentals/dedup.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python
-#!/usr/bin/python
 #-*- coding: utf-8 -*-
 #===============================================================================
-#
 #         FILE: dedup.py
-#
 #        USAGE: ./ 
-#
 #  DESCRIPTION: dedup a project to remove duplicates  
-#
-#
 #      OPTIONS: ---
 # REQUIREMENTS: ---
 #         BUGS: ---
@@ -20,16 +13,10 @@
 #      CREATED:  
 #     REVISION: Tue Jun 20 13:11:19 PDT 2017
 #===============================================================================
-
-# -*- coding: iso-8859-15 -*-
-
 import pandas as pd
 import numpy as np
 
-
-# This is going to be a demonstration of the dedupping capabilities in python
-# and pandas
-
+# Demonstration of the dedupping capabilities in python and pandas
 print("\nFirst we are going to create some dummy data\n")
 #We need to have several rows that have repeating infomration in them 
 df = pd.DataFrame({ 'A' : 1.,
@@ -41,9 +28,6 @@ df = pd.DataFrame({ 'A' : 1.,
 
 print(df)
 
-print("""\nOkay, now that we have some data to work with we are going to be
-        dedupping it, we will eliminate the duplicate values of the test row and
-        the 'train' row, That way we will only have unique rows. \nNeat.\n""")
 df2 = df.drop_duplicates('E')
 
 print(df2 )
